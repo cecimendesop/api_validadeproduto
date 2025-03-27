a22700a443343a9db31974870c26923f8d272dd6
cecimendesop <cecilia.mopaulo@gmail.com>
2025-03-27 11:09:45 -0300

    sgrter

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify
+from flask_pydantic_spec import FlaskPydanticSpec
+import datetime
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+app = Flask(__name__)
+
+spec = FlaskPydanticSpec( 'flask',
+                          title = 'Flask API - SENAI',
+                          version = '1.0.0',)
+spec.register(app)
+
+@app.route('/<tipo>/<quantidade>/')
+def validacao(tipo, quantidade):
+    prazo = int(quantidade)
+    meses = datetime.today()+relativedelta(months=prazo)
+    #anos
+    anos = datetime.today()+relativedelta(years=prazo)
+    #semanas
+    semanas = datetime.today()+relativedelta(weeks=prazo)
+    #dias
+    dias = datetime.today()+relativedelta(days=prazo)
+
+    if tipo == 'meses':
+        validade = meses
+    elif tipo == 'anos':
+        validade = anos
+    elif tipo == 'semanas':
+        validade = semanas
+    elif tipo == 'dias':
+        validade = dias
+
+    return jsonify ({
+        "Fabricação em": datetime.today().strftime('%d/%m/%Y'),
+        "Validade": validade.strftime('%d/%m/%Y'),
+    })
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
